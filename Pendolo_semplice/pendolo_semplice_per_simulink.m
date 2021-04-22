d89234c5e4470e9a97d51d609818ccf480ca9613
oleggenova <82159658+oleggenova@users.noreply.github.com>
2021-04-22 11:06:27 +0200

    x

---
+++ Pendolo_semplice/pendolo_semplice_per_simulink.m
@@ -0,0 +1,39 @@
+%modello in SS
+%grandezze fisiche
+M=0.5;
+m=0.2;
+b=0.1;
+l=0.3;
+i=0.006;
+g=9.8;
+
+%modello come fdt
+q=(M+m)*(i+m*l^2)-(m*l)^2;
+num=[m*l/q 0];
+den=[1 b*(i+m*l^2)/q -(M+m)*m*g*l/q -b*m*g*l/q];
+pend=tf(num,den);
+
+t=[0:0.001:1];
+impulse(pend,t)
+rlocus(pend)
+
+%rete correttrice per luogo radici
+contr=tf(1,[1 0]);
+rlocus(contr*pend);
+%sigrid(0.92) comando obsoleto
+
+%aggiunta di poli e zeri
+z1=3; z2=4; 
+p1=0; %polo che compensa zero in origine
+p2=60; %polo lontano
+
+numc=conv([1 z2],[1 z1]);
+denc=conv([1 p2],[1 p1]);
+contr1=tf(num1,den1);
+rlocus(contr1*pend)
+
+[k,poles]=rlocfind(contr1*pend);
+sys_cl=feedback(pend,contr1*k);
+impulse(sys_cl)
+
+
