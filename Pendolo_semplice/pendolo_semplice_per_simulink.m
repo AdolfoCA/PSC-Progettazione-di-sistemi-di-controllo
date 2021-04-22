3b0c59d7d1ec42eb834f366bf94aa8deea1192f4
oleggenova <82159658+oleggenova@users.noreply.github.com>
2021-04-22 17:41:55 +0200

    s

--- Pendolo_semplice/pendolo_semplice_per_simulink.m
+++ Pendolo_semplice/pendolo_semplice_per_simulink.m
@@ -1,10 +1,10 @@
 %modello in SS
 %grandezze fisiche
-M=0.5;
-m=0.2;
-b=0.1;
-l=0.3;
-i=0.006;
+M=0.5; %massa carrello
+m=0.2;  %massa pendolo
+b=0.1;  %attrito carrello-pavimento
+l=0.3;  %lunghezza pendolo
+i=0.006; %inerzia pendolo
 g=9.8;
 
 %modello come fdt
@@ -27,13 +27,16 @@ z1=3; z2=4;
 p1=0; %polo che compensa zero in origine
 p2=60; %polo lontano
 
+% rete correttrice migliorata
 numc=conv([1 z2],[1 z1]);
 denc=conv([1 p2],[1 p1]);
 contr1=tf(numc,denc);
 rlocus(contr1*pend)
 
+%prelevo gain per la rete correttrice
 [k,poles]=rlocfind(contr1*pend);
 sys_cl=feedback(pend,contr1*k);
 impulse(sys_cl)
+
 
 
