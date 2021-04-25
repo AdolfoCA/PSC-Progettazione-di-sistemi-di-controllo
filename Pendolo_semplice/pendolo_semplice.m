de9a3f18940d76ec5066b62cb42679d6faad51a1
GioLat <gio.lat4@gmail.com>
2021-04-25 18:22:54 +0200

    pendolo con carretto

--- Pendolo_semplice/pendolo_semplice.m
+++ Pendolo_semplice/pendolo_semplice.m
@@ -6,14 +6,14 @@ close all
 %Pendolo linearizzato range di lavoro 0°%13°, 0rad%0.23rad
 g=9.81; %accelerazione gravitazionale [m/s^2]
 theta=0.10; %angolo iniziale [rad]
-d=1; %lunghezza asta [m], regolare il proporzionale in base all'asta
+d=2; %lunghezza asta [m], regolare il proporzionale in base all'asta
 x=theta*d; %posizione iniziale [m]
 v_x=0; %velocità iniziale [m/s]
 a_x=g*x/abs(d); %accelerazione iniziale [m/s^2]
 pos=[x d*cos(x/d)]; %coordinate x y del pendolo
 h=0.025; %passo
 kp=15;  %proporzionale 
-ki=1; %integrativa
+ki=2; %integrativa
 e=0; %errore
 intgr=0; %memoria
 rif=0; %posizione desiderata
@@ -38,9 +38,7 @@ for t=0:h:300
    end
    
    if mod(t,10)==0 %introduco in disturbo randomico
-         v_x=v_x+abs(rand-0.7);
+         v_x=v_x+abs(rand-0.8);
          print="Introduco un disturbo..."
-   end
-   
-   
+   end   
 end
