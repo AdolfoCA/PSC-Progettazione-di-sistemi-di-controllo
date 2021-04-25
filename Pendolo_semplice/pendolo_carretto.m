877a5e841241de9814240e70e9f60a04948b3cb7
GioLat <gio.lat4@gmail.com>
2021-04-25 18:29:57 +0200

    pendolo con carretto

--- Pendolo_semplice/pendolo_carretto.m
+++ Pendolo_semplice/pendolo_carretto.m
@@ -5,8 +5,8 @@ close all
 
 %Pendolo linearizzato range di lavoro 0°%13°, 0rad%0.23rad
 g=9.81; %accelerazione gravitazionale [m/s^2]
-theta=0.20; %angolo iniziale [rad]
-d=0.5; %lunghezza asta [m], regolare il proporzionale in base all'asta
+theta=0.1; %angolo iniziale [rad]
+d=1; %lunghezza asta [m], regolare il proporzionale in base all'asta
 xc=0; %posizione iniziale carretto [m]
 v_xc=0; %velocità iniziale carretto [m/s]
 a_xc=0; %accelerazione iniziale carretto [m/s^2]
@@ -25,14 +25,20 @@ rif=0; %posizione desiderata del pendolo rispetto al carretto
 for t=0:h:300
    pos_p=[xp d*cos(xp/d)]; %aggiorno posizione
    
-   figure(1) %plot che vede l'asta dal punto di vista del carretto
-   plot([0 pos_p(1,1)], [0 pos_p(1,2)],'r-') %plot pendolo
-   xlim([-2*abs(d) 2*abs(d)]); %utili al plot
-   ylim([-2*abs(d) 2*abs(d)]); %utili al plot
-   title("Pendolo visto dal carretto");
+%    figure(1) %plot che vede l'asta dal punto di vista del carretto
+%    plot([0 pos_p(1,1)], [0 pos_p(1,2)],'r-') %plot pendolo
+%    xlim([-2*abs(d) 2*abs(d)]); %utili al plot
+%    ylim([-2*abs(d) 2*abs(d)]); %utili al plot
+%    title("Pendolo visto dal carretto");
+  
    figure(2) %plot che vede il carretto dal punto di vista di un osservatore esterno
    plot(xc,0,'sb','MarkerSize',50) %plot carretto
+   hold on
+   plot([xc xc+xp],[0 d*cos(xp/d)],'r-'); %plot pendolo
+   hold off
    title("Carretto visto dall'esterno");
+   xlim([-3*d 3*d]); %NB: i disturbi possono portare il carretto ad uscire dai limiti del plot
+   ylim([-2*d 2*d]);
    drawnow %utili al plot
    
    e=rif-xp; %calcolo errore posizione del pendolo rispetto al CM del carretto
@@ -47,17 +53,16 @@ for t=0:h:300
    xc=xc+v_xc*h; %posizione del carretto   
    xp=xp+v_xp*h; %aggiorno coordinata x del pendolo rispetto al carretto
    
-   if abs(v_xp)<= 0.05 && abs(e)<=0.1 %resetto memoria
+   if abs(v_xp)<= 0.03 && abs(e)<=0.5 %resetto memoria
        intgr=0; %serve per impedire che il pendolo cada
    end
    
-%    if mod(t,10)==0 %introduco in disturbo randomico
-%        if rand()>0.5  
-%             v_xp=v_xp+rand()/10
-%        else
-%             v_xp=v_xp-rand()/10
-%        end  
-%        print="Introduco un disturbo..."
-%    end
-   
+   if mod(t,10)==0 %introduco in disturbo randomico
+       if rand()>0.5  
+            v_xp=v_xp+rand()/10;
+       else
+            v_xp=v_xp-rand()/10;
+       end  
+       print="Introduco un disturbo..."
+   end
 end
