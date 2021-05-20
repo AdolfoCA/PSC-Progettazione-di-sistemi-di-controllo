081038b45e6d2889c1943c3f70548377fc76eef0
oleggenova <82159658+oleggenova@users.noreply.github.com>
2021-05-20 11:03:54 +0200

    script matlab per comunicazione su seriale

---
+++ Serial_script.m
@@ -0,0 +1,20 @@
+%% Instrument Connection
+
+% Find a serial port object.
+obj1 = instrfind('Type', 'serial', 'Port', 'COM3', 'Tag', '');
+
+% Create the serial port object if it does not exist
+% otherwise use the object that was found.
+if isempty(obj1)
+    obj1 = serial('COM3');
+else
+    fclose(obj1);
+    obj1 = obj1(1);
+end
+
+% Connect to instrument object, obj1.
+fopen(obj1);
+
+
+
+%%
